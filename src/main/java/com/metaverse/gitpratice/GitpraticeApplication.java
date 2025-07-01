76f0dc8e8d8afc00d8cf235d9a8f576ffa3b96d7
MINEUNCHAE <c389074@g.hongik.ac.kr>
2025-07-01 15:30:11 +0900

    chore: First Init

---
+++ src/main/java/com/metaverse/gitpratice/GitpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.gitpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitpraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaverse/gitpratice/GitpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.gitpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
